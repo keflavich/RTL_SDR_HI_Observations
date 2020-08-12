f68cf1ca3ffdefa9b2c2c155a3aa4bedac0401a4
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2020-08-12 16:44:35 -0400

    clean up integration script

--- RunAnIntegrationIPYNB.py
+++ RunAnIntegrationIPYNB.py
@@ -1,27 +1,61 @@
-import subprocess, sys
-import tqdm.notebook
+import time
+import datetime
+import subprocess
+import sys
 
-tint = 60
-altitude = 80
-azimuth = 180
-for ii in tqdm.notebook.tqdm(range(24*(3600//tint))):
-    assert 0 == subprocess.call([r'C:\Users\gluner\Anaconda3\Library\bin\bias_tee_on.bat'])
+
+def record_integration(altitude, azimuth, tint, observatory_longitude=-82.3,
+                       observatory_latitude=29.6, sleep_time=120):
+    """
+    Record a single integration
+
+    Parameters
+    ----------
+    altitude : float, deg
+        The altitude the telescope is pointing at
+    azimuth : float, deg
+        The azimuth the telescope is pointing toward
+    tint : int, seconds
+        The integration time to run (how long are you recording)
+    observatory_latitude : float
+    observatory_longitude : float
+        Coordinates of the observatory (default: Gainesville)
+    sleep_time : int, seconds
+        The amount of time to sleep in the case that the USB dongle is
+        unresponsive.  If this case comes up often, you may need to unplug the
+        dongle and let it cool
+    """
+
+    response = subprocess.call([r'C:\Users\gluner\Anaconda3\Library\bin\bias_tee_on.bat'])
+    if response != 0:
+        raise IOError("Failed to turn the bias tee (the thing that powers the low-noise amplifier (LNA)) on.  "
+                      f"Error value was {response}")
+
+    # https://github.com/keflavich/1420SDR/blob/master/1420_psd.py
     proc = subprocess.Popen([sys.executable,
                             '1420_psd.py', '-i', str(tint),
-                             '--do_fsw', '--obs_lon=-82.3', '--obs_lat=29.6',
+                             '--do_fsw',
+                             f'--obs_lon={observatory_longitude}',
+                             f'--obs_lat={observatory_latitude}',
                              f'--altitude={altitude}',
                              f'--azimuth={azimuth}',
                              '--freqcorr=60'])
+    # wait for  the integration to complete
     time.sleep(tint)
     print(datetime.datetime.now())
+
     try:
         outs, errs = proc.communicate(timeout=tint)
-    except TimeoutExpired:
+    except subprocess.TimeoutExpired:
         proc.kill()
         outs, errs = proc.communicate()
-        print(f"Killed task at {datetime.datetime.now()}.   Turning off for 2 minutes.")
+        print(f"The RTL-SDR failed to respond, so we're turning it off and waiting {sleep_time} seconds.")
+        print(f"Killed task at {datetime.datetime.now()}.")
         print(f"outs={outs}, errs={errs}")
-        assert 0 == subprocess.call([r'C:\Users\gluner\Anaconda3\Library\bin\bias_tee_off.bat'])
-        time.sleep(120)
+        response = subprocess.call([r'C:\Users\gluner\Anaconda3\Library\bin\bias_tee_off.bat'])
+        if response != 0:
+            raise IOError("Failed to turn the bias tee (the thing that powers the low-noise amplifier (LNA)) off.  "
+                          f"Error value was {response}")
+
+        time.sleep(sleep_time)
         print(f"Resuming integration at {datetime.datetime.now()}")
-    assert 0 == subprocess.call([r'C:\Users\gluner\Anaconda3\Library\bin\bias_tee_on.bat'])
